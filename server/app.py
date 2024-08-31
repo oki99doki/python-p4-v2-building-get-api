55d4052e8025e159153c23e82858e959c99551d6
Stefan Klinkert <s.klinkert@gmail.com>
2024-08-31 13:28:39 +0200

    complete Building GET API CodeAlong

--- server/app.py
+++ server/app.py
@@ -11,8 +11,10 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = False
 
+# Initilizing Migration Object
 migrate = Migrate(app, db)
 
+# Itializing Database Connection or Database Object within Flask Application
 db.init_app(app)
 
 @app.route('/')
@@ -20,6 +22,72 @@ def index():
     return "Index for Game/Review/User API"
 
 # start building your API here
+@app.route('/games')
+def games():
+    
+    games = []
+    #games_by_title = Game.query.order_by(Game.title).all()
+    #first_2_games = Game.query.limit(2).all()
+    #for game in first_2_games:
+    #for game in games_by_title:
+    # for game in Game.query.all():
+    #     # game_dict = {
+    #     #     "title": game.title,
+    #     #     "genre": game.genre,
+    #     #     "platform": game.platform,
+    #     #     "price": game.price,
+    #     # }
+    #     game_dict = game.to_dict()
+    #     games.append(game_dict)
+
+    games = [game.to_dict() for game in Game.query.all()]
+
+    response = make_response(
+        games,
+        200
+        # 200,
+        #{"Content-Type": "application/json"}
+    )
+
+    return response
+
+
+@app.route('/games/<int:id>')
+def game_by_id(id):
+    game = Game.query.filter(Game.id == id).first()
+
+    # game_dict = {
+    #     "title": game.title,
+    #     "genre": game.genre,
+    #     "platform": game.platform,
+    #     "price": game.price,
+    # }
+
+    game_dict = game.to_dict()
+
+    response = make_response(
+        game_dict,
+        200
+    )
+
+    return response
+
+
+@app.route('/games/users/<int:id>')
+def game_users_by_id(id):
+    game = Game.query.filter(Game.id == id).first()
+    users = []
+    for review in game.reviews:
+        user = review.user
+        user_dict = user.to_dict(rules=("-reviews",))
+        users.append(user_dict)
+
+    response = make_response(
+        users,
+        200
+    )
+
+    return response
 
 
 if __name__ == '__main__':
